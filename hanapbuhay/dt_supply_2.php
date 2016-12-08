b826f803d123b83ef19da62b3ae43784ff6dc31b
kevinolea123 <dankevinolea@gmail.com>
2016-12-08 14:18:42 +0800

    Update dt_supply_2.php

--- hanapbuhay/dt_supply_2.php
+++ hanapbuhay/dt_supply_2.php
@@ -4,129 +4,114 @@
  * Copyright: 2012 - John Becker, Beckersoft, Inc.
  * Copyright: 2010 - Allan Jardine
  * License:   GPL v2 or BSD (3-point)
- */
-  
+ */ 
 class TableData {
- 
-  private $_db;
-  public function __construct() {
-    require("../l33tz9.php");
-  }
-  public function get($table, $index_column, $columns) {
-    // Paging
-    $sLimit = "";
-    if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' ) {
-      $sLimit = "LIMIT ".intval( $_GET['iDisplayStart'] ).", ".intval( $_GET['iDisplayLength'] );
-    }
-    
-    // Ordering
-    $sOrder = "";
-    if ( isset( $_GET['iSortCol_0'] ) ) {
-      $sOrder = "ORDER BY  ";
-      for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ ) {
-        if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" ) {
-          $sortDir = (strcasecmp($_GET['sSortDir_'.$i], 'ASC') == 0) ? 'ASC' : 'DESC';
-          $sOrder .= "`".$columns[ intval( $_GET['iSortCol_'.$i] ) ]."` ". $sortDir .", ";
-        }
-      }
-      
-      $sOrder = substr_replace( $sOrder, "", -2 );
-      if ( $sOrder == "ORDER BY" ) {
-        $sOrder = "";
-      }
-    }
-    
-    /* 
-     * Filtering
-     * NOTE this does not match the built-in DataTables filtering which does it
-     * word by word on any field. It's possible to do here, but concerned about efficiency
-     * on very large tables, and MySQL's regex functionality is very limited
-     */
-    $sWhere = "";
-    if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" ) {
-      $sWhere = "WHERE (";
-      for ( $i=0 ; $i<count($columns) ; $i++ ) {
-        if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" ) {
-          $sWhere .= "`".$columns[$i]."` LIKE :search OR ";
-        }
-      }
-      $sWhere = substr_replace( $sWhere, "", -3 );
-      $sWhere .= ')';
-    }
-    
-    // Individual column filtering
-    for ( $i=0 ; $i<count($columns) ; $i++ ) {
-      if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' ) {
-        if ( $sWhere == "" ) {
-          $sWhere = "WHERE ";
-        }
-        else {
-          $sWhere .= " AND ";
-        }
-        $sWhere .= "`".$columns[$i]."` LIKE :search".$i." ";
-      }
-    }
-    
-    // SQL queries get data to display
-    if ($_SESSION['filter']=="NPMO") {
-      $sQuery = "SELECT SQL_CALC_FOUND_ROWS DISTINCT t.sector, count(t.supplyrefid) as avail, m.region FROM PRTsupplytags t LEFT JOIN PRTsupply m ON t.supplyrefid=m.id GROUP BY t.sector";
-    } else {
-      $sQuery = "SELECT SQL_CALC_FOUND_ROWS DISTINCT t.sector, count(t.supplyrefid) as avail, GROUP_CONCAT(DISTINCT m.province SEPARATOR ', ') provinces, m.region FROM PRTsupplytags t LEFT JOIN PRTsupply m ON t.supplyrefid=m.id WHERE region='".$_SESSION['filter']."' GROUP BY t.sector".$sWhere." ".$sOrder." ".$sLimit;
-    }
-    
-    $statement = $this->_db->prepare($sQuery);
-    
-    // Bind parameters
-    if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" ) {
-      $statement->bindValue(':search', '%'.$_GET['sSearch'].'%', PDO::PARAM_STR);
-    }
-    for ( $i=0 ; $i<count($columns) ; $i++ ) {
-      if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' ) {
-        $statement->bindValue(':search'.$i, '%'.$_GET['sSearch_'.$i].'%', PDO::PARAM_STR);
-      }
-    }
-    $statement->execute();
-    $rResult = $statement->fetchAll();
-    
-    $iFilteredTotal = current($this->_db->query('SELECT FOUND_ROWS()')->fetch());
-    
-    // Get total number of rows in table
-    $sQuery = "SELECT COUNT(`".$index_column."`) FROM `".$table."`";
-    $iTotal = current($this->_db->query($sQuery)->fetch());
-    
-    // Output
-    $output = array(
-      "sEcho" => intval($_GET['sEcho']),
-      "iTotalRecords" => $iTotal,
-      "iTotalDisplayRecords" => $iFilteredTotal,
-      "aaData" => array()
-    );
-    
-    // Return array of values
-    foreach($rResult as $aRow) {
-      $row = array();     
-      for ( $i = 0; $i < count($columns); $i++ ) {
-        if ( $columns[$i] == "version" ) {
-          // Special output formatting for 'version' column
-          $row[] = ($aRow[ $columns[$i] ]=="0") ? '-' : $aRow[ $columns[$i] ];
-        }
-        else if ( $columns[$i] != ' ' ) {
-          $row[] = $aRow[ $columns[$i] ];
-        }
-      }
-      $output['aaData'][] = $row;
-    }
-    
-    echo json_encode( $output );
-  }
+ 	private $_db;
+	public function __construct() {
+		require("../l33tz9.php");
+	}
+	public function get($table, $index_column, $columns) {
+		// Paging
+		$sLimit = "";
+		if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' ) {
+			$sLimit = "LIMIT ".intval( $_GET['iDisplayStart'] ).", ".intval( $_GET['iDisplayLength'] );
+		}
+		// Ordering
+		$sOrder = "";
+		if ( isset( $_GET['iSortCol_0'] ) ) {
+			$sOrder = "ORDER BY  ";
+			for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ ) {
+				if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" ) {
+					$sortDir = (strcasecmp($_GET['sSortDir_'.$i], 'ASC') == 0) ? 'ASC' : 'DESC';
+					$sOrder .= "`".$columns[ intval( $_GET['iSortCol_'.$i] ) ]."` ". $sortDir .", ";
+				}
+			}	
+			$sOrder = substr_replace( $sOrder, "", -2 );
+			if ( $sOrder == "ORDER BY" ) {
+				$sOrder = "";
+			}
+		}
+		/* 
+		 * Filtering
+		 * NOTE this does not match the built-in DataTables filtering which does it
+		 * word by word on any field. It's possible to do here, but concerned about efficiency
+		 * on very large tables, and MySQL's regex functionality is very limited
+		 */
+		$sWhere = "";
+		if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" ) {
+			$sWhere = "WHERE (";
+			for ( $i=0 ; $i<count($columns) ; $i++ ) {
+				if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" ) {
+					$sWhere .= "`".$columns[$i]."` LIKE :search OR ";
+				}
+			}
+			$sWhere = substr_replace( $sWhere, "", -3 );
+			$sWhere .= ')';
+		}
+		// Individual column filtering
+		for ( $i=0 ; $i<count($columns) ; $i++ ) {
+			if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' ) {
+				if ( $sWhere == "" ) {
+					$sWhere = "WHERE ";
+				}
+				else {
+					$sWhere .= " AND ";
+				}
+				$sWhere .= "`".$columns[$i]."` LIKE :search".$i." ";
+			}
+		}
+		// SQL queries get data to display
+		if ($_SESSION['filter']=='NPMO') {
+			$sQuery = "SELECT m.id, t.tag, t.sector, m.firstname, m.lastname, m.sex, m.birthdate, m.province, m.municipality, m.status, m.education, m.pantawidid, m.hasNSO, m.hasNBI, GROUP_CONCAT(DISTINCT t.tag SEPARATOR ', ') as subsectors FROM PRTsupplytags t LEFT JOIN PRTsupply m ON t.supplyrefid=m.id WHERE t.sector='".$_SESSION['sector']."' GROUP BY m.id";
+		} else {
+			$sQuery = "SELECT m.id, t.tag, t.sector, m.firstname, m.lastname, m.sex, m.birthdate, m.province, m.municipality, m.status, m.education, m.pantawidid, m.hasNSO, m.hasNBI, GROUP_CONCAT(DISTINCT t.tag SEPARATOR ', ') as subsectors FROM PRTsupplytags t LEFT JOIN PRTsupply m ON t.supplyrefid=m.id WHERE REGION = '".$_SESSION['filter']."' AND t.sector='".$_SESSION['sector']."' GROUP BY m.id";
+		}
+		$statement = $this->_db->prepare($sQuery);
+		// Bind parameters
+		if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" ) {
+			$statement->bindValue(':search', '%'.$_GET['sSearch'].'%', PDO::PARAM_STR);
+		}
+		for ( $i=0 ; $i<count($columns) ; $i++ ) {
+			if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' ) {
+				$statement->bindValue(':search'.$i, '%'.$_GET['sSearch_'.$i].'%', PDO::PARAM_STR);
+			}
+		}
+		$statement->execute();
+		$rResult = $statement->fetchAll();
+		$iFilteredTotal = current($this->_db->query('SELECT FOUND_ROWS()')->fetch());
+		// Get total number of rows in table
+		$sQuery = "SELECT COUNT(`".$index_column."`) FROM `".$table."`";
+		$iTotal = current($this->_db->query($sQuery)->fetch());
+		// Output
+		$output = array(
+			"sEcho" => intval($_GET['sEcho']),
+			"iTotalRecords" => $iTotal,
+			"iTotalDisplayRecords" => $iFilteredTotal,
+			"aaData" => array()
+		);
+		// Return array of values
+		foreach($rResult as $aRow) {
+			$row = array();			
+			for ( $i = 0; $i < count($columns); $i++ ) {
+				if ( $columns[$i] == "version" ) {
+					// Special output formatting for 'version' column
+					$row[] = ($aRow[ $columns[$i] ]=="0") ? '-' : $aRow[ $columns[$i] ];
+				}
+				else if ( $columns[$i] != ' ' ) {
+					$row[] = $aRow[ $columns[$i] ];
+				}
+			}
+			$output['aaData'][] = $row;
+		}
+		echo json_encode( $output );
+	}
 }
 header('Pragma: no-cache');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 // Create instance of TableData class
-
 $table_data = new TableData();
 // Get the data
-$table_data->get('PRTsupplytags', 'id', array('sector', 'avail', 'region'));
+$table_data->get('PRTsupply', 'id', array('id', 'firstname', 'lastname', 'subsectors', 'sex', 'birthdate', 'education', 'province', 'municipality','sector','pantawidid', 'hasNSO', 'hasNBI','status', 'tag'));
 /*
  * Alternatively, you may want to use the same class for several differnt tables for different pages.
  * By adding something similar to the following to your .htaccess file you can control this a little more...
@@ -137,9 +122,9 @@ $table_data->get('PRTsupplytags', 'id', array('sector', 'avail', 'region'));
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         if (isset($_REQUEST['_page'])) {
-          if($_REQUEST['_page'] === 'PAGENAME') {
-              $table_data->get('table_name', 'index_column', array('column1', 'column2', 'columnN'));
-          }
+        	if($_REQUEST['_page'] === 'PAGENAME') {
+	            $table_data->get('table_name', 'index_column', array('column1', 'column2', 'columnN'));
+	        }
         }
         break;
     default:
